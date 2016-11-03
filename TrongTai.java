afeba978d904d76d07fcb68b3118c6082c504c89
Tran Hoang Long <longtranf@gmail.com>
2016-11-03 04:01:47 +0000

    Trong Tai fix 4

--- TrongTai.java
+++ TrongTai.java
@@ -5,14 +5,26 @@
  */
 package testSQuares2;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author VPC
  */
 class TrongTai {
+
+    private final int DUNG = 0;
+    private final int DI = 1;
+    private final int AN = 2;
+    private final int BOC = 3;
+    private final int KIEM_TRA = 4;
+    private final int DOI = 5;
+
+    private final int NUMBER_IMAGE = 25;
 
     private Game game;
     public Player player;
@@ -22,6 +34,10 @@ class TrongTai {
     private int x, y;         //Tọa độ bàn tay
     Step buocDi;
     int selected;
+    int direction;
+    int state;
+    boolean eating = false;
+    int count = 0; // bien dem de ve ban tay
 
     // Short Link
     House[] houseShortLink;
@@ -40,6 +56,129 @@ class TrongTai {
         q6ShortLink = game.board.q6;
         p1ShortLink = game.p1;
         p2ShortLink = game.p2;
+        this.state = DOI;
+        this.count = 0;
+    }
+
+    /**
+     * Di Tung buoc
+     */
+    public void handle(Step buocDi) {
+
+        // neu trang thai dang tu doi
+        // duoc goi
+        // den luot rai quan
+        if (this.state == DOI) {
+            this.selected = buocDi.chose;
+            this.direction = buocDi.direc;
+            this.danInHand = layQuan(this.selected);
+            chuyenNhaKe(this.direction);
+            System.out.println("So Dan : " + this.danInHand + " Selected : " + this.selected + " direction : " + this.direction);
+            this.state = DI;
+            setToaDo();
+            return;
+        }
+
+        if (this.count < NUMBER_IMAGE) {
+            tangToaDo();
+            return;
+        }
+        this.count = 0;
+
+        try {
+            Thread.sleep(300);
+        } catch (Exception e) {
+        }
+        // kiem tra trang thai cua trong tai
+        switch (this.state) {
+
+            case DUNG:
+                this.state = DOI;
+                this.eating = false;
+                setTurnToken(buocDi);
+                if (!checkContinueGame(board)) {
+                    this.game.turnToken = 0;
+                    return;
+                }
+                if (checkBoardPlayer(this.game.turnToken)) {
+                    themDan(this.game.turnToken);
+                }
+                resetBuocDi();
+
+                break;
+
+            case DI:
+                rai1Quan();
+                chuyenNhaKe(direction);
+                System.out.println("So Dan : " + this.danInHand + " Selected : " + this.selected + " direction : " + this.direction);
+                setToaDo();
+                break;
+
+            case AN:
+                An(buocDi, this.selected);
+                chuyenNhaKe(direction);
+                this.eating = true;
+                this.state = KIEM_TRA;
+                break;
+
+            case BOC:
+
+                break;
+
+            case KIEM_TRA:
+                int result = checkFinalHouse(this.selected, direction);
+                System.out.println("Check : Selected : " + this.selected + " Direction : " + this.direction);
+                System.out.println("Ket Qua Kiem Tra : " + result);
+                switch (result) {
+
+                    // Truong hop di tiep
+                    case 1:
+                        if (this.eating) {
+                            this.state = DUNG;
+                            break;
+                        }
+                        this.danInHand = layQuan(this.selected);
+                        chuyenNhaKe(direction);
+                        this.state = DI;
+                        break;
+
+                    // Truong hop An
+                    case 2:
+                        chuyenNhaKe(direction);
+                        this.state = AN;
+                        break;
+
+                    // Truong hop dung
+                    case 0:
+                        this.state = DUNG;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                break;
+
+        }
+
+        return;
+    }
+
+    /**
+     * Rai 1 Quan Neu So Quan Trong tay het Chuyen trang thai ban ve kiem tra
+     */
+    public void rai1Quan() {
+        this.danInHand--;
+        if (this.selected != 6 && this.selected != 0) {
+            houseShortLink[this.selected].tangDanSo();
+        } else if (this.selected == 0) {
+            q0ShortLink.tangDanSo();
+        } else {
+            q6ShortLink.tangDanSo();
+        }
+        if (this.danInHand == 0) {
+            this.state = KIEM_TRA;
+        }
     }
 
     /**
@@ -47,7 +186,7 @@ class TrongTai {
      *
      * @param buocDi (chose, direc)
      */
-    public void handle(Step buocDi) {
+    public void oldhandle(Step buocDi) {
 
         int soDan;
         this.selected = buocDi.chose;
@@ -88,7 +227,7 @@ class TrongTai {
         // Trong Truong hop khong du quan de rai khi het dan trong cac o
         // Score < 5
         // thi game over
-        if (checkBoardPlayer(this.game.turnToken)) {
+        if (this.game.turnToken != 0 && checkBoardPlayer(this.game.turnToken)) {
             themDan(this.game.turnToken);
         }
 
@@ -193,6 +332,12 @@ class TrongTai {
         if (checkEmpty(checked)) {
             return 0;
         } else {
+            if (checked == 0 && q0ShortLink.coQuan && q0ShortLink.getDanSo() < 5) {
+                return 0;
+            }
+            if (checked == 6 && q6ShortLink.coQuan && q6ShortLink.getDanSo() < 5) {
+                return 0;
+            }
             return 2;
         }
     }
@@ -375,9 +520,73 @@ class TrongTai {
         p2ShortLink.resetBuocDi();
     }
 
-    public void paint(Graphics2D g2d) {
-        //g2d.drawImage(Game.voSoi, x, y - 50, null);
-        
-                
+    public void setToaDo() {
+        if (this.direction == 1) {
+            if (this.selected < 6 && this.selected != 0) {
+                this.x = game.board.START_X + (this.selected - 2) * 100;
+                this.y = game.board.START_Y;
+
+            } else if (this.selected > 6) {
+                this.x = game.board.START_X + (11 - this.selected + 1) * 100;
+                this.y = game.board.START_Y + 102;
+            } else if (this.selected == 0) {
+                this.x = game.board.START_X - 100;
+                this.y = game.board.START_Y + 50;
+            } else {
+                this.x = game.board.START_X + 503;
+                this.y = game.board.START_Y + 50;
+            }
+        }
+        else {
+            if (this.selected < 6 && this.selected != 0) {
+                this.x = game.board.START_X + this.selected * 100;
+                this.y = game.board.START_Y;
+
+            } else if (this.selected > 6) {
+                this.x = game.board.START_X + (11 - this.selected - 1) * 100;
+                this.y = game.board.START_Y + 102;
+            } else if (this.selected == 0) {
+                this.x = game.board.START_X - 100;
+                this.y = game.board.START_Y + 50;
+            } else {
+                this.x = game.board.START_X + 503;
+                this.y = game.board.START_Y + 50;
+            }
+        }
     }
+
+    public void tangToaDo() {
+        if (this.direction == -1) {
+            this.count++;
+            if (this.selected > 6) {
+                this.x += 4;
+            } else if (this.selected < 6 && this.selected != 0) {
+                this.x -= 4;
+            } else {
+                setToaDo();
+            }
+        } else {
+            this.count++;
+            if (this.selected > 6) {
+                this.x -= 4;
+            } else if (this.selected < 6 && this.selected != 0) {
+                this.x += 4;
+            } else {
+                setToaDo();
+            }
+        }
+    }
+
+    public void paint(Graphics2D g2d) {
+
+        if (this.state == DI) {
+
+            g2d.drawImage(Game.namTay, this.x, this.y - 50, null);
+            g2d.setColor(Color.yellow);
+            g2d.drawString(String.valueOf(danInHand), this.x+43 , this.y-50+38);
+
+        }
+
+    }
+
 }
